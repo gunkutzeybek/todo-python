7c500864dcfd0feb36304572bf152c822845fb4b
gunkut <gunkut.zeybek@gmail.com>
2018-10-17 16:08:47 +0300

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, Client
 from django.urls import resolve
-from django.http import HttpRequest
 from lists.models import Item, List
 
 from lists.views import home_page 
@@ -19,37 +18,6 @@ class HomePageTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.Client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-class ListAndItemModelTest(TestCase):
-    Client = Client()
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
     Client = Client()
